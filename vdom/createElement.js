19a4e340a0552830d267fdb16cc9e5f7ca94a461
LiJiajun <1814023573@qq.com>
2020-04-12 18:27:32 +0800

    vdom完善,diff整合入patchC

--- vdom/createElement.js
+++ vdom/createElement.js
@@ -1,25 +1,54 @@
-function createElement (tag,props,children){
-    this.tag = tag
-    this.props = props
-    this.children = children
-}
-
-createElement.prototype.render = function(){
-    let el = document.createElement(this.tag);
-    for(let key in this.props){
-        el.setAttribute(key,this.props[key])
+const vnode = (tags,props,key,children,text)=>{
+    return {
+        tags,
+        props,
+        key,
+        children,
+        text
     }
-    if(this.children){
-        this.children.forEach(child => {
-            child = (child instanceof createElement) ?child.render():  document.createTextNode(child)
-            el.appendChild(child)
-        });
-    }
-    return el
 }
-// module.exports= (tag,props,children) => new createElement(tag,props,children)
-
-
-//虽然简单，但应当清楚:
-//createElement函数不能使用箭头函数定义，这是箭头函数的this指向导致，需要额外关注这一点。
-//而在箭头函数内部调用构造函数，则不影响构造函数内部this的指向在new实例化对象过程中的改变。
+function createElement (tags,props={},...children){
+    //获取属性的key值，然后删掉
+    let key
+    if(props.key){
+        key = props.key
+        delete props.key
+    }
+    //判断子节点是标签还是文本
+    //这里因为没把children赋值导致．．．浪费很多时间
+    children =  children.map(child=>{
+        if(typeof (child) == 'string'){
+            return vnode(undefined,undefined,undefined,undefined,child)
+        } else {
+            return child
+        }
+    })
+    return vnode(tags,props,key,children)
+    // this.tag = tag
+    // this.props = props
+    // this.children = children
+}
+const createDomElementVnode = (vnode)=>{
+    let {
+        tags,
+        props,
+        key,
+        children,
+        text
+    } = vnode
+    if(tags) {
+        vnode.domElement = document.createElement(tags)
+        //根据我们虚拟节点的属性，更新真实dom
+        updateProps(vnode)
+        //递归调用渲染
+        children.forEach(childNode=>render(childNode,vnode.domElement))
+    } else {
+        vnode.domElement = document.createTextNode(text)
+        console.log(vnode.domElement)
+    }
+    return vnode.domElement
+}
+const render= (vnode,container)=>{
+    let ele = createDomElementVnode(vnode)
+    container.appendChild(ele)
+}
--- vdom/patch.js
+++ vdom/patch.js
@@ -1,87 +1,167 @@
-const patch = (node,patches)=>{
-    let walker = {index : 0}
-    dfswalks(node,walker,patches)
-}
-const dfswalks = (node,walker,patches)=>{
-    let itemPatches = patches[walker.index]
-    let len = node.children?
-    node.children.length:0
-    for(let i = 0;i < len;i++){
-        let child = node.children[i]
-        walker.index++
-        console.log(walker.index)
-        dfswalks(child,walker,patches)
+const patch=(oldVnode,newVnode)=>{   
+    //判断类型不同
+    if(oldVnode.tags&&oldVnode.tags!=newVnode.tags){
+        return oldVnode.domElement.parentNode.replaceChild(
+            createDomElementVnode(newVnode),
+            oldVnode.domElement
+        )
     }
-    if(itemPatches){
-        applyPatches(node,itemPatches)
+    //类型相同，换文本
+    if(oldVnode.text){
+        if(oldVnode.text == newVnode.text) return
+        return oldVnode.domElement.textContent = newVnode.text
     }
-}
-const applyPatches = (node,itemPatches)=>{
-    itemPatches.forEach(itemPatch=>{
-        switch(itemPatch.change){
-            case 'replace':
-                let newNode = (typeof itemPatch.node == 'string')?
-                document.createTextNode(itemPatch.node): itemPatch.node.render()
-                node.parentNode.replaceChild(newNode,node)
-                break
-            case 'reorder':
-                reorderChildren(node,itemPatch.moves)
-                break
-            case 'props':
-                setProps(node,itemPatch)
-                break
-            case 'text':
-                if(node.textContent){
-                node.textContent = itemPatch.text
-                } else {
-                    node.nodeValue = itemPatch.text
-                }
-                break
-            default:
-                throw new Error('unknown patch change' + itemPatch.change)
-        }
-    })
-}
-const setProps = (node,itemPatch)=>{
-    for(let key in itemPatch.props){
-        if(itemPatch.props[key] == void 666){
-            node.removeAttribute(key)
-        }else {
-            let value = itemPatch.props[key]
-            node.setAttribute(key,value)
+    let domElement = newVnode.domElement = oldVnode.domElement
+    updateProps(newVnode,oldVnode.props)
+    //对比儿子
+    //1.旧的有儿子，新的有儿子
+    //2.旧的有儿子　新的无儿子
+    //3.新增儿子
+    let oldChildren = oldVnode.children
+    let newChildren = newVnode.children
+
+    if(oldChildren.length>0 && newChildren.length>0){
+        //1.
+        updateChildren(domElement,oldChildren,newChildren)
+    }else if(oldChildren.length>0){
+        domElement.innerHTML = ''
+    }else if (newChildren.length>0){
+        //新增儿子，转成dom转进去
+        for(let i = 0; i < newChildren.length; i++){
+            domElement.appendChild(createDomElementVnode(newChildren[i]))
         }
     }
 }
-// module.exports = patch
-function reorderChildren (node, moves) {
-    var staticNodeList =Array.prototype.slice.call(node.children)
-    var maps = {}
+//新旧属性的对比
+const updateProps = (newVnode,oldProps={})=>{
+    let domElement = newVnode.domElement//真实dom
+    let newProps = newVnode.props//当前节点中的属性
+    //新旧作对比
+    //1.旧的有，新的没有，属性直接删除
+    for(let oldPropname in oldProps){
+        if(!newProps[oldPropname]){
+            delete domElement[oldPropname]
+        }
+    }
+    //2.旧的没有，新的有
+    for(let newPropsName in newProps){
+        domElement[newPropsName] = newProps[newPropsName]
+    }
+    //3.style
+    let newStyleObj = newProps.style||{}
+    let oldStyleObj = oldProps.style||{}
+    for(let propName in oldStyleObj){
+        if(!newStyleObj[propName]){
+            domElement.style[propName] = '' 
+        }
+    }
+    //循环属性给dom
+    for(let newPropsName in newProps){
+        //如果有style
+        if(newPropsName == 'style'){
+            let styleObj = newProps.style
+            for(let s in styleObj){
+                domElement.style[s] = styleObj[s]
+            }
+        } else {
+            domElement[newPropsName] = newProps[newPropsName]
+        }
+    }
+}
+
+
+const isSameVnode = (oldVnode,newVnode)=>{
+    return oldVnode.key === newVnode.key && oldVnode.tags === newVnode.tags 
+}
+//创建映射表{a:0,b:1,c:3}
+const keyMapByIndex = oldChildren =>{
+    let map = {}
+    for(let i = 0; i<oldChildren.length;i++){
+        let current = oldChildren[i]
+        if(current.key){
+            map[current.key] = i
+        }
+    }
+    return map
+}
+const updateChildren = (parent,oldChildren,newChildren)=>{
+
+    let oldStartIndex = 0
+    let oldStartVnode = oldChildren[0]
+    let oldEndIndex = oldChildren.length - 1
+    let oldEndVnode = oldChildren[oldEndIndex]
+    
+    let newStartIndex = 0
+    let newStartVnode = newChildren[0]
+    let newEndIndex = newChildren.length - 1
+    let newEndVnode = newChildren[newEndIndex]
+    //判断旧孩子和新孩子，循环的时候，谁先结束就停止
+    while(oldStartIndex <= oldEndIndex && newStartIndex <=newEndIndex){
+        if(!oldStartVnode){
+            oldStartVnode = oldChildren[++oldStartIndex]
+        } else if(!oldEndVnode){                    
+            oldEndVnode = oldChildren[--oldEndIndex]
+        } 
+        //如果标签和key相同接着往下走
+        if(isSameVnode(oldStartVnode,newStartVnode)){
+            console.log('首指针相同')
+            //标签相同．比较属性
+            patch(oldStartVnode,newStartVnode)
+            //如果他们两个一样，往后走
+            oldStartVnode = oldChildren[++oldStartIndex]
+            newStartVnode = newChildren[++newStartIndex]
+        } else if(isSameVnode(oldEndVnode,newEndVnode)){
+            console.log('旧尾指针与新尾指针相同')
+            patch(oldEndVnode,newEndVnode)
+            oldEndVnode = oldChildren[--oldEndIndex]
+            newEndVnode = newChildren[--newEndIndex]
+        } else if(isSameVnode(oldStartVnode,newEndVnode)){
+            console.log('旧首指针与新尾指针相同')
+            patch(oldStartVnode,newEndVnode)
+            parent.insertBefore(oldStartVnode.domElement,oldEndVnode.domElement.nextSibling)
+            //nextSibling．．．拼错了被坑的好惨
+            oldStartVnode = oldChildren[++oldStartIndex]
+            newEndVnode =newChildren[--newEndIndex]
+        } else if(isSameVnode(oldEndIndex,newStartIndex)){
+            console.log('旧尾部指针与新首指针相同')
+            patch(oldEndVnode,newStartVnode)
+            parent.insertBefore(oldEndVnode.domElement,oldStartVnode.domElement)
+            oldEndVnode = oldChildren[--oldEndIndex]
+            newStartVnode =newChildren[++newStartIndex]
+        } else {
+            //暴力对比
+            let index = map[newStartVnode.key]
+            if(index == null){
+                parent.insertBefore(createDomElementVnode(newStartVnode),oldStartVnode.domElement)
+            } else {
+                let toMoveNode = oldChildren[index]
+                patch(toMoveNode,newStartVnode)
+                parent.insertBefore(toMoveNode.domElement,oldStartVnode.domElement)
+                oldChildren[index] = undefined
+            }
+            //移动位置
+            newStartVnode = newChildren[++newStartIndex]
+        }
+    }
+    //把多余的节点放进去，只有小于或者等于，才说明有剩余
+    if(newStartIndex <= newEndIndex){
+        for(let i = newStartIndex; i <= newEndIndex; i++) {
+            
+            // parent.appendChild(createDomElementVnode(newChildren[i]))
+            let beforeElement = newChildren[newEndIndex+1]==null?null:
+            newChildren[newEndIndex+1].domElement
+            parent.insertBefore(createDomElementVnode(newChildren[i]),beforeElement)
+            //当before为null,相当于append使用   
+        }
+    }
+
   
-    staticNodeList.forEach (node => {
-      if (node.nodeType === 1) {
-        var key = node.getAttribute('key')
-        if (key) {
-          maps[key] = node
+    //判断中间的undefined
+    if(oldStartIndex <= oldEndIndex){
+        for(let i = oldStartIndex;i<=oldEndIndex;i++){
+            if(oldChildren[i]){
+                parent.removeChild(oldChildren[i].domElement)
+            }
         }
-      }
-    })
-    if(moves){
-    moves.forEach(move=>{
-      var index = move.index
-      if (move.type === 0) { 
-        if (staticNodeList[index] === node.children[index]) { 
-          node.removeChild(node.children[index])
-        }
-        staticNodeList.splice(index, 1)
-      } else if (move.type === 1) {
-        let insertNode = maps[move.item.key]
-          ? maps[move.item.key].cloneNode(true) 
-          : (typeof move.item === 'object')
-              ? move.item.render()
-              : document.createTextNode(move.item)
-        staticNodeList.splice(index, 0, insertNode)
-        node.insertBefore(insertNode, node.children[index] || null)
-      }
-    })
+    }
 }
-  }
